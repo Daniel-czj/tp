3018353b731d29e6a7c24642f56795b118648553
Daniel Chua <e1384443@u.nus.edu>
2026-03-16 01:38:32 +0800

    Fix ExitCommand and update FitLogger main method to use new parameters

--- src/main/java/seedu/FitLogger/ExitCommand.java
+++ src/main/java/seedu/FitLogger/ExitCommand.java
@@ -1,10 +1,38 @@
 package seedu.FitLogger;
 
+/**
+ * Command that saves current workout data and exits the application.
+ */
 public class ExitCommand extends Command {
-    public void execute(Ui ui) {
-        System.out.println("execute function run");
+    private final Storage storage;
+    private final WorkoutList workouts;
+
+    /**
+     * Creates an {@code ExitCommand} that persists the current workout list before exit.
+     *
+     * @param storage Storage handler used to save workout data.
+     * @param workouts Workout list to be saved.
+     */
+    public ExitCommand(Storage storage, WorkoutList workouts) {
+        this.storage = storage;
+        this.workouts = workouts;
     }
 
+    /**
+     * Saves workout data and displays a goodbye message to the user.
+     *
+     * @param ui UI used to display output messages.
+     */
+    public void execute(Ui ui) {
+        storage.saveData(workouts.getWorkouts());
+        ui.showGoodbye();
+    }
+
+    /**
+     * Indicates that this command should terminate the application loop.
+     *
+     * @return {@code true} always.
+     */
     public boolean isExit() {
         return true;
     }
--- src/main/java/seedu/FitLogger/FitLogger.java
+++ src/main/java/seedu/FitLogger/FitLogger.java
@@ -3,10 +3,14 @@ package seedu.FitLogger;
 public class FitLogger {
     private Ui ui;
     private Parser parser;
+    private Storage storage;
+    private WorkoutList workouts;
 
     public FitLogger() {
         ui = new Ui();
         parser = new Parser();
+        storage = new Storage(); // temp: please change
+        workouts = new WorkoutList(); // temp: please change
     }
 
     public void run() {
@@ -19,8 +23,8 @@ public class FitLogger {
             String[] workoutInfo = parser.parse(command);
 
             //later remove this line after u change the line above
-            Command c = new ExitCommand();
-
+            Command c = new ExitCommand(storage, workouts);
+            
             c.execute(ui);
             isExit = c.isExit();
         }
