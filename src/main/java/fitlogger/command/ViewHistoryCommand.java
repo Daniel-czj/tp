bc1fa51d80cf1f32049598e3c4af9232a64f9f77
Javierseow <seowjackiejavier@gmail.com>
2026-03-20 02:18:48 +0800

    Remove unnecessary String.valueOf

--- src/main/java/fitlogger/command/ViewHistoryCommand.java
+++ src/main/java/fitlogger/command/ViewHistoryCommand.java
@@ -15,7 +15,7 @@ public class ViewHistoryCommand extends Command {
         ui.showMessage("Here's your past exercises");
         ui.showLine();
         for (int i = 0; i < workouts.getSize(); i++) {
-            ui.showMessageNoNewline(String.valueOf(i+1) + ". ");
+            ui.showMessageNoNewline(i + 1 + ". ");
             ui.printWorkout(workouts.getWorkoutAtIndex(i));
         }
         ui.showLine();
